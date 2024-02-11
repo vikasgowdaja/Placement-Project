b304f7be90044ac8236d71337797297aa9dd8ac4
Vikas Gowda J A <vikasgowddaja@gmail.com>
2024-02-11 16:11:13 +0530

    First Commit

---
+++ src/com/cg/placement/repository/IPlacementRepository.java
@@ -0,0 +1,20 @@
+package com.cg.placement.repository;
+
+import com.cg.placement.entities.Placement;
+
+public interface IPlacementRepository {
+	public Placement addPlacement(Placement placement);
+
+	public Placement updatePlacement(Placement placement);
+
+	public Placement searchPlacement(int id);
+
+	public Placement deletePlacement(Placement placement);
+
+	public abstract void beginTransaction();
+
+	public abstract void commitTrasaction();
+
+	void deletePlacement(int id);
+
+}
